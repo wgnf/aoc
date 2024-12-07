b41d401a727673305cfa3102cb121c0e8c099f62
Martin Wagenführ <martin@wgnf.de>
2024-12-08 00:03:26 +0100

    add day 1 from 2024

--- src/AdventOfCode.2024/2024/Day1/Day1.cs
+++ src/AdventOfCode.2024/2024/Day1/Day1.cs
@@ -1,24 +1,59 @@
 ﻿using AdventOfCode.Common;
+using AdventOfCode.Common.Utils;
 
 namespace AdventOfCode._2024._2024.Day1;
 
 // ReSharper disable UnusedType.Global
 
-[PuzzleDay(Day = 1, Title = "...", ExpectedSampleResultPart1 = "...", ExpectedSampleResultPart2 = "...")]
-public sealed class Day1 : IDaySolver
+[Puzzle(Day = 1, Title = "Historian Hysteria", ExpectedSampleResultPart1 = "11", ExpectedSampleResultPart2 = "31")]
+public sealed class Day1 : IPuzzleSolver
 {
+    private List<int> _leftElements = [];
+    private List<int> _rightElements = [];
+
     public void Init(IEnumerable<string> inputLines)
     {
-        throw new NotImplementedException();
+        _leftElements = [];
+        _rightElements = [];
+
+        foreach (var line in inputLines)
+        {
+            var subParts = line.SplitTrimRemoveEmpty("   ");
+
+            _leftElements.Add(int.Parse(subParts[0]));
+            _rightElements.Add(int.Parse(subParts[1]));
+        }
+
+        _leftElements.Sort();
+        _rightElements.Sort();
     }
 
     public string SolvePart1()
     {
-        throw new NotImplementedException();
+        var totalDistance = 0;
+
+        for (var index = 0; index < _leftElements.Count; index++)
+        {
+            var distance = Math.Abs(_leftElements[index] - _rightElements[index]);
+
+            totalDistance += distance;
+        }
+
+        return totalDistance.ToString();
     }
 
     public string SolvePart2()
     {
-        throw new NotImplementedException();
+        var similarityScore = 0;
+
+        foreach (var leftElement in _leftElements)
+        {
+            var occurences = _rightElements.Count(rightElement => rightElement == leftElement);
+            var similarity = occurences * leftElement;
+
+            similarityScore += similarity;
+        }
+
+        return similarityScore.ToString();
     }
 }
--- src/AdventOfCode.Common/Utils/StringExtensions.cs
+++ src/AdventOfCode.Common/Utils/StringExtensions.cs
@@ -6,4 +6,9 @@ public static class StringExtensions
     {
         return input.Split(splitChar, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
     }
+
+    public static string[] SplitTrimRemoveEmpty(this string input, string splitChar)
+    {
+        return input.Split(splitChar, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    }
 }
