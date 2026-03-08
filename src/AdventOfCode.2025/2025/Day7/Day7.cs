1dfe3882ac3d103cbd30cccbfd09676217f07658
Martin Wagenführ <martin@wgnf.de>
2026-03-08 17:11:07 +0100

    2025 day7 wip

--- src/AdventOfCode.2025/2025/Day7/Day7.cs
+++ src/AdventOfCode.2025/2025/Day7/Day7.cs
@@ -9,84 +9,123 @@ namespace AdventOfCode._2025._2025.Day7;
 public sealed class Day7 : IPuzzleSolver
 {
     // ReSharper disable once IdentifierTypo
-    private Matrix<char> _taychonManifold = new(0, 0);
+    private TaychonManifold _taychonManifold = new([]);
     
     public void Init(IEnumerable<string> inputLines)
     {
-        _taychonManifold = Matrix<char>.FromInputLines(inputLines, c => c);
+        _taychonManifold = new TaychonManifold(inputLines);
     }
 
     public string SolvePart1()
     {
-        var splits = 0;
-        var start = _taychonManifold.AsEnumerable().FirstOrDefault(x => x.Value == 'S');
-        var seenSplitPositions = new HashSet<Position>();
-        var beamStartPositions = new Queue<Position>();
-        beamStartPositions.Enqueue(start.Position);
+        _taychonManifold.Solve();
 
-        while (beamStartPositions.Count > 0)
-        {
-            var beamStartPosition = beamStartPositions.Dequeue();
-            
-            var collected = _taychonManifold.CollectInDirection(beamStartPosition, Direction.South, (_, element) => element.Value != '^').ToList();
-            var lastElement = collected.Last();
-            if (lastElement.Value != '^')
-            {
-                continue;
-            }
-
-            if (!seenSplitPositions.Add(lastElement.Position))
-            {
-                continue;
-            }
-
-            splits++;
-                
-            var newPositionLeft = lastElement.Position.AdjustBasedOnDirection(Direction.West);
-            var newPositionRight = lastElement.Position.AdjustBasedOnDirection(Direction.East);
-                
-            beamStartPositions.Enqueue(newPositionLeft);
-            beamStartPositions.Enqueue(newPositionRight);
-        }
-
-        return splits.ToString();
+        return _taychonManifold.Splits.Count.ToString();
     }
 
     public string SolvePart2()
     {
-        var splits = 0;
-        var start = _taychonManifold.AsEnumerable().FirstOrDefault(x => x.Value == 'S');
-        var seenSplitPositions = new HashSet<Position>();
-        var beamStartPositions = new Queue<Position>();
-        beamStartPositions.Enqueue(start.Position);
+        _taychonManifold.Solve();
 
-        while (beamStartPositions.Count > 0)
+        var allEndingBeams = _taychonManifold.Beams.Where(x => x.EndSplit == null);
+
+        var realities = allEndingBeams.Sum(x => GetPossibilities(x, 0));
+        return realities.ToString();
+    }
+
+    private long GetPossibilities(Beam beam, long localPossibilities)
+    {
+        var origins = beam.OriginSplits;
+        if (origins.Any(x => x.IsStart))
         {
-            var beamStartPosition = beamStartPositions.Dequeue();
-            
-            var collected = _taychonManifold.CollectInDirection(beamStartPosition, Direction.South, (_, element) => element.Value != '^').ToList();
-            var lastElement = collected.Last();
-            if (lastElement.Value != '^')
-            {
-                continue;
-            }
-
-            if (!seenSplitPositions.Add(lastElement.Position))
-            {
-                continue;
-            }
-
-            splits++;
-                
-            var newPositionLeft = lastElement.Position.AdjustBasedOnDirection(Direction.West);
-            var newPositionRight = lastElement.Position.AdjustBasedOnDirection(Direction.East);
-                
-            beamStartPositions.Enqueue(newPositionLeft);
-            beamStartPositions.Enqueue(newPositionRight);
+            return localPossibilities + 1;
         }
 
-        var timelines = (splits * 2) - 2;
+        foreach (var origin in origins)
+        {
+            var beamsOfOrigin = _taychonManifold.Beams.Where(x => x.EndSplit == origin);
+            localPossibilities += beamsOfOrigin.Count();
+            localPossibilities += GetPossibilities(beam, localPossibilities);
+        }
 
-        return timelines.ToString();
+        return 0;
+    }
+
+    private class TaychonManifold
+    {
+        private readonly Matrix<char> _taychonManifold;
+
+        public TaychonManifold(IEnumerable<string> inputLines)
+        {
+            _taychonManifold = Matrix<char>.FromInputLines(inputLines, c => c);
+        }
+
+        public List<Beam> Beams { get; } = [];
+
+        public List<Split> Splits { get; } = [];
+
+        public void Solve()
+        {
+            var start = _taychonManifold.AsEnumerable().FirstOrDefault(x => x.Value == 'S');
+
+            var thingsToProcess = new Queue<(Split Origin, List<Position> BeamStartPositions)>();
+            var startSplit = new Split { IsStart = true, Position = start.Position, };
+            thingsToProcess.Enqueue((startSplit, [start.Position]));
+        
+            while (thingsToProcess.Count > 0)
+            {
+                var thingToProcess = thingsToProcess.Dequeue();
+
+                foreach (var beamStartPosition in thingToProcess.BeamStartPositions)
+                {
+                    var foundBeam = Beams.FirstOrDefault(x => x.StartPosition == beamStartPosition);
+                    if (foundBeam == null)
+                    {
+                        foundBeam = new Beam { StartPosition = beamStartPosition };
+                        foundBeam.OriginSplits.Add(thingToProcess.Origin);
+                        Beams.Add(foundBeam);   
+                    }
+                    
+                    var collected = _taychonManifold.CollectInDirection(foundBeam.StartPosition, Direction.South, (_, element) => element.Value != '^').ToList();
+                    var lastElement = collected.Last();
+                    if (lastElement.Value != '^')
+                    {
+                        continue;
+                    }
+            
+                    var seenSplit = Splits.FirstOrDefault(x => x.Position == lastElement.Position);
+                    if (seenSplit != null)
+                    {
+                        foundBeam.EndSplit = seenSplit;
+                        continue;
+                    }
+
+                    var split = new Split { Position = lastElement.Position };
+                    Splits.Add(split);
+                    foundBeam.EndSplit = split;
+
+                    var newPositionLeft = lastElement.Position.AdjustBasedOnDirection(Direction.West);
+                    var newPositionRight = lastElement.Position.AdjustBasedOnDirection(Direction.East);
+                    
+                    thingsToProcess.Enqueue((split, [newPositionLeft, newPositionRight]));
+                }
+            }
+        }
+    }
+
+    private class Beam
+    {
+        public Position StartPosition { get; init; }
+        
+        public HashSet<Split> OriginSplits { get; } = [];
+        
+        public Split? EndSplit { get; set; }
+    }
+
+    private class Split
+    {
+        public bool IsStart { get; set; }
+        
+        public Position Position { get; init; }
     }
 }
